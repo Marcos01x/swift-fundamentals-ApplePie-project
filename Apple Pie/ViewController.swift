544030df6290e9561139dd3548b9c10299388fe1
Marcos Arroyo <marcosarroyog@gmail.com>
2023-11-05 22:59:21 -0600

    add variables of the game

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -13,13 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet var correctWordLabel: UILabel!
     @IBOutlet var scoreLabel: UILabel!
     @IBOutlet var letterButtons: [UIButton]!
-    
-    
+
+    var listOfWords = ["buccaneer","swift","glorius","incandescent","bug","program"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0
+    var totalLosses = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
